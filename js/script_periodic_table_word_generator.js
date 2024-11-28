5598e0cab09e5333f23e9152c93c6e058d742a11
mikail-eliyah-00 <eliyah.mikail@gmail.com>
2024-11-28 14:04:36 -0800

    a18: periodic table word generator

---
+++ js/script_periodic_table_word_generator.js
@@ -0,0 +1,162 @@
+const path_to_images = '../images/periodic_table_elements/';
+
+const periodicTable = {
+    H: path_to_images + 'hydrogen.png',
+    HE: path_to_images + 'helium.png',
+    LI: path_to_images + 'lithium.png',
+    BE: path_to_images + 'beryllium.png',
+    B: path_to_images + 'boron.png',
+    C: path_to_images + 'carbon.png',
+    N: path_to_images + 'nitrogen.png',
+    O: path_to_images + 'oxygen.png',
+    F: path_to_images + 'fluorine.png',
+    NE: path_to_images + 'neon.png',
+    NA: path_to_images + 'sodium.png',
+    MG: path_to_images + 'magnesium.png',
+    AL: path_to_images + 'aluminum.png',
+    SI: path_to_images + 'silicon.png',
+    P: path_to_images + 'phosphorus.png',
+    S: path_to_images + 'sulfur.png',
+    CL: path_to_images + 'chlorine.png',
+    AR: path_to_images + 'argon.png',
+    K: path_to_images + 'potassium.png',
+    CA: path_to_images + 'calcium.png',
+    SC: path_to_images + 'scandium.png',
+    TI: path_to_images + 'titanium.png',
+    V: path_to_images + 'vanadium.png',
+    CR: path_to_images + 'chromium.png',
+    MN: path_to_images + 'manganese.png',
+    FE: path_to_images + 'iron.png',
+    CO: path_to_images + 'cobalt.png',
+    NI: path_to_images + 'nickel.png',
+    CU: path_to_images + 'copper.png',
+    ZN: path_to_images + 'zinc.png',
+    GA: path_to_images + 'gallium.png',
+    GE: path_to_images + 'germanium.png',
+    AS: path_to_images + 'arsenic.png',
+    SE: path_to_images + 'selenium.png',
+    BR: path_to_images + 'bromine.png',
+    KR: path_to_images + 'krypton.png',
+    RB: path_to_images + 'rubidium.png',
+    SR: path_to_images + 'strontium.png',
+    Y: path_to_images + 'yttrium.png',
+    ZR: path_to_images + 'zirconium.png',
+    NB: path_to_images + 'niobium.png',
+    MO: path_to_images + 'molybdenum.png',
+    TC: path_to_images + 'technetium.png',
+    RU: path_to_images + 'ruthenium.png',
+    RH: path_to_images + 'rhodium.png',
+    PD: path_to_images + 'palladium.png',
+    AG: path_to_images + 'silver.png',
+    CD: path_to_images + 'cadmium.png',
+    IN: path_to_images + 'indium.png',
+    SN: path_to_images + 'tin.png',
+    SB: path_to_images + 'antimony.png',
+    I: path_to_images + 'iodine.png',
+    XE: path_to_images + 'xenon.png',
+    CS: path_to_images + 'cesium.png',
+    BA: path_to_images + 'barium.png',
+    LA: path_to_images + 'lanthanum.png',
+    CE: path_to_images + 'cerium.png',
+    PR: path_to_images + 'praseodymium.png',
+    ND: path_to_images + 'neodymium.png',
+    PM: path_to_images + 'promethium.png',
+    SM: path_to_images + 'samarium.png',
+    EU: path_to_images + 'europium.png',
+    GD: path_to_images + 'gadolinium.png',
+    TB: path_to_images + 'terbium.png',
+    DY: path_to_images + 'dysprosium.png',
+    HO: path_to_images + 'holmium.png',
+    ER: path_to_images + 'erbium.png',
+    TM: path_to_images + 'thulium.png',
+    YB: path_to_images + 'ytterbium.png',
+    LU: path_to_images + 'lutetium.png',
+    HF: path_to_images + 'hafnium.png',
+    TA: path_to_images + 'tantalum.png',
+    W: path_to_images + 'tungsten.png',
+    RE: path_to_images + 'rhenium.png',
+    OS: path_to_images + 'osmium.png',
+    IR: path_to_images + 'iridium.png',
+    PT: path_to_images + 'platinum.png',
+    AU: path_to_images + 'gold.png',
+    HG: path_to_images + 'mercury.png',
+    TL: path_to_images + 'thallium.png',
+    PB: path_to_images + 'lead.png',
+    BI: path_to_images + 'bismuth.png',
+    PO: path_to_images + 'polonium.png',
+    AT: path_to_images + 'astatine.png',
+    RN: path_to_images + 'radon.png',
+    FR: path_to_images + 'francium.png',
+    RA: path_to_images + 'radium.png',
+    AC: path_to_images + 'actinium.png',
+    TH: path_to_images + 'thorium.png',
+    PA: path_to_images + 'protactinium.png',
+    U: path_to_images + 'uranium.png',
+    NP: path_to_images + 'neptunium.png',
+    PU: path_to_images + 'plutonium.png',
+    AM: path_to_images + 'americium.png',
+    CM: path_to_images + 'curium.png',
+    BK: path_to_images + 'berkelium.png',
+    CF: path_to_images + 'californium.png',
+    ES: path_to_images + 'einsteinium.png',
+    FM: path_to_images + 'fermium.png',
+    MD: path_to_images + 'mendelevium.png',
+    NO: path_to_images + 'nobelium.png',
+    LR: path_to_images + 'lawrencium.png',
+    RF: path_to_images + 'rutherfordium.png',
+    DB: path_to_images + 'dubnium.png',
+    SG: path_to_images + 'seaborgium.png',
+    BH: path_to_images + 'bohrium.png',
+    HS: path_to_images + 'hassium.png',
+    MT: path_to_images + 'meitnerium.png',
+    DS: path_to_images + 'darmstadtium.png',
+    RG: path_to_images + 'roentgenium.png',
+    CN: path_to_images + 'copernicium.png',
+    UUT: path_to_images + 'ununtrium.png',
+    UUP: path_to_images + 'ununpentium.png',
+    UUS: path_to_images + 'ununseptium.png',
+    UUO: path_to_images + 'ununoctium.png'
+};
+
+function generateImages() {
+    const inputText = document.getElementById('inputText').value;
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '';
+
+    let i = 0;
+    while (i < inputText.length) {
+        let added = false;
+
+        let twoChar = (inputText[i] + inputText[i + 1]).toUpperCase();
+        if (periodicTable[twoChar]) {
+            // It's a valid two-character symbol, so process it.
+            const img = document.createElement('img');
+            img.src = periodicTable[twoChar];
+            img.alt = twoChar;
+            img.className = 'element';
+            outputDiv.appendChild(img);
+            i += 2; // Skip the next character since we've processed this pair
+            added = true;
+        }
+
+        // Check if single-letter symbols match
+        if (!added && periodicTable[inputText[i].toUpperCase()]) {
+            const img = document.createElement('img');
+            img.src = periodicTable[inputText[i].toUpperCase()];
+            img.alt = inputText[i].toUpperCase();
+            img.className = 'element';
+            outputDiv.appendChild(img);
+            i++; // Increment by one character for single-character match
+            added = true;
+        }
+
+        // If no match, add the character as a span (for special characters, spaces, etc.)
+        if (!added) {
+            const span = document.createElement('span');
+            span.textContent = inputText[i];
+            span.className = 'element';
+            outputDiv.appendChild(span);
+            i++; // Increment by one character for non-matching character
+        }
+    }
+}
